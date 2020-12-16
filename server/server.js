697bb4f80922604fa05dc3e130554a6d85017a1e
Max Bogaers <mbogaers@studnet.avans.nl>
2020-12-16 16:11:50 +0100

    migration from old repo

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const compression = require('compression')
+
+const app = express()
+
+// Compress static assets to enhance performance.
+// Decrease the download size of your app through gzip compression:
+app.use(compression())
+
+//
+// appname is the name of the "defaultProject" value that was set in the angular.json file.
+// When built in production mode using 'ng build --prod', a ./dist/{appname} folder is
+// created, containing the generated application. The appname points to that folder.
+//
+// Replace the name below to match your own "defaultProject" value!
+//
+const appname = '@soombtcloob/app'
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'www')))+
+
+// Catch all routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'www', 'index.html'))
+})
+
+// Get port from environment and store in Express.
+const port = process.env.PORT || '4200'
+app.set('port', port)
+// Create HTTP server.
+const server = http.createServer(app)
+// Listen on provided port, on all network interfaces.
+server.listen(port, () => {
+  console.log(`Angular app \'${appname}\' running in ${process.env.NODE_ENV} mode on port ${port}`)
+})
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'pharma-partners-front-end'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('pharma-partners-front-end');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('pharma-partners-front-end app is running!');
+  });
+});
