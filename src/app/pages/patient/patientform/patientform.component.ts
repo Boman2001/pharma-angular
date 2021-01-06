7547cbbb1c686968ae92d05654c04e89463cb2f6
ZiasLuijkx <zhm.luijkx@student.avans.nl>
2021-01-06 11:32:28 +0100

    Patientform added

---
+++ src/app/pages/patient/patientform/patientform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patientform',
+  templateUrl: './patientform.component.html',
+  styleUrls: ['./patientform.component.css']
+})
+export class PatientformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
