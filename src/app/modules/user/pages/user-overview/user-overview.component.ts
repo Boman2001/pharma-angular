f2ea5d934341fd59810dae59b8785113db0e2dee
Thomas Terlaak <to.terlaak@outlook.com>
2021-01-12 12:11:41 +0100

    Implemented Datatable in prescription and user overview.

--- src/app/modules/user/pages/user-overview/user-overview.component.ts
+++ src/app/modules/user/pages/user-overview/user-overview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { BaseEntity } from "src/app/modules/core/core.module";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: "app-user-overview",
@@ -7,7 +10,47 @@ import { Component, OnInit } from "@angular/core";
 })
 export class UserOverviewComponent implements OnInit {
 
-  constructor() { }
+  headerArray: string[] = [
+    "Name",
+    "Email",
+    "Dob",
+    "Gender",
+    "PhoneNumber",
+    "City",
+    "Street",
+    "HouseNumber",
+    "HouseNumberAddon",
+    "PostalCode"
+
+  ];
+  actionsArray: { id: string, class: string, icon: string, action: (entity: BaseEntity) => void }[] = [
+    {
+      id: "user-detail",
+      class: "btn btn-primary",
+      icon: '<i class="fas fa-eye"></i>',
+      action: (entity: BaseEntity) => {
+        this.router.navigate(['/' + entity.Id])
+      }
+    },
+    {
+      id: "user-edit",
+      class: "btn btn-warning",
+      icon: '<i class="fas fa-pencil-alt"></i>',
+      action: (entity: BaseEntity) => {
+        this.router.navigate(['/' + entity.Id + '/edit'])
+      }
+    },
+    {
+      id: "user-delete",
+      class: "btn btn-danger",
+      icon: '<i class="fas fa-trash-alt"></i>',
+      action: (entity: BaseEntity) => {
+        console.log(entity.Id);
+      }
+    }
+  ]
+
+  constructor(public userService: UserService, public router: Router) { }
 
   ngOnInit(): void {
   }
