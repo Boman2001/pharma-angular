b463f3e378d8c0d9d97738f9c65e2cea9676d82c
DESKTOP-1F5CJ9K\Youri van der Sande <y.vandersande@hotmail.com>
2021-01-20 14:43:36 +0100

    Consult visit additional examination page done

--- src/app/modules/examination/models/additional-examination-result.model.ts
+++ src/app/modules/examination/models/additional-examination-result.model.ts
@@ -1,11 +1,13 @@
 import { BaseEntity } from "../../core/core.module";
 import { Consultation } from "../../consultation/consultation.module";
 import { Patient } from "../../patient/patient.module";
+import {AdditionalExaminationType} from "./additional-examination-type.model";
 
 
 export class AdditionalExaminationResult extends BaseEntity {
   consultation: Consultation;
+  additionalExaminationType: AdditionalExaminationType;
   patient: Patient;
   value: string;
-  time: Date;
+  date: string;
 }
